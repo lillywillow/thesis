9dc1e365c50c0f898a99bd32a1b2573cffbb301e
lillywillow <pinkhairedsheep@gmail.com>
2021-06-22 13:13:18 +0200

    group 2

--- code/data-wrangling.py
+++ code/data-wrangling.py
@@ -30,60 +30,91 @@ def convert_all(show_paths, path = codes):
                 convert_single(episode, code)
 
 def convert_single(episode, code):
-    with open(episode, encoding = "UTF-8") as episode:
+    with open(episode, encoding = "UTF-8") as epi:
     # converts the HTML format to text
-        soup = bs(episode, features="lxml")
+        soup = bs(epi, features="lxml")
         raw_content = soup.get_text()
-        episode_path = episode.name
     raw_content = wrangle(raw_content, code)
     # wrangles the text
-    with open(episode_path.replace("html_source", "txt_conversion") + ".txt", "w", encoding = "UTF-8") as output_file:
+    with open(episode.replace("html_source", "txt_conversion") + ".txt", "w", encoding = "UTF-8") as output_file:
     # writes down the wrangled text
         output_file.write(raw_content)
 
 def wrangle(content, code):
-    def group0(content):
+    def group0(content, colon):
     # default group
         return content
 
-    def group1(content):
-    # ATL, KOR
+    def group1(content, colon):
+    # ('ATL', 'KOR')
         content = max(content.split("\n\n\n\n\n\n"), key = len)
         # strips the beginning of the transcript
         content = content.split("\nCast\n")[0].strip()
         # strips the end of the transcript
+        if colon:
+            return colonize(content)
+        else:
+            return content.strip()
+
+    def group2(content, colon):
+    # ('ADV', 'CLW', 'DUC', 'FUT', 'OWL', 'PPG', 'SHE', 'SPO', 'VOL')
+        content = content.split("Retrieved from \"")[0].strip()
+        if (len(re.split(r'Futurama transcripts', content)) > 1):
+            content = (re.split(r'Futurama transcripts', content))[0].strip()
+        if (content.endswith("See Also: Episode Transcript List")):
+            content = content.split("See Also: Episode Transcript List")[0].strip()
+        if (len(content.split("Play Sound")) > 1):
+            content = content.split("Play Sound")[1].strip()
+        if (len(content.split("The Neutral Planet")) > 1):
+            content = content.split("The Neutral Planet")[1].strip()
+        if (len(re.split(r'Next: "[a-zA-Z0-9,\' !\/]*"', content, re.M)) > 1):
+            content = re.split(r'Next: "[a-zA-Z0-9,\' !\/]*"', content, re.M)[1].strip()
+        if (len(re.split(r'\nTranscripts?\n', content, re.M)) > 2):
+            content = re.split(r'\nTranscripts?\n', content, re.M)[2].strip()
+        if (len(re.split(r'This article is a transcript of the SpongeBob SquarePants episode .+\n', content)) > 1):
+            content = re.split(r'This article is a transcript of the SpongeBob SquarePants episode .+\n', content)[1].strip()
+        if (len(re.split(r'\(Opening shot: .+\n', content)) > 1):
+            content = re.split(r'\(Opening shot: .+\n', content)[1].strip()
+        if (len(re.split(r'\n([A-Z]+: )', content, re.M)) > 2):
+            content = re.split(r'\n([A-Z]+: )', content, maxsplit = 1)[1] + re.split(r'\n([A-Z]+:)', content, maxsplit = 1)[2].strip()
+        if colon:
+            return colonize(content)
+        else:
+            return content.strip()
+
+    def colonize(content):
+    # converts columns to "<speaker>: <sentence>"
         lines = content.splitlines()
         lines = list(filter(None, lines))
         content = ""
         for line in lines:
-            if re.match(r'(.*)?[a-z:]$', line.lower()):
-                content += line + ": "
+            if re.match(r'(.*)?(([a-z:])|(b\.o\.y\.d\.))$', line.lower().strip()):
+                content += line.strip() + ": "
             else:
-                content += line + "\n"
-
+                content += line.strip() + "\n"
         return content.strip()
     
     switcher = {
-        'ADV': group0,
-        'ATL': group1,
-        'CLW': group0,
-        'DUC': group0,
-        'FUT': group0,
-        'GRA': group0,
-        'GUM': group0,
-        'KOR': group1,
-        'MLP': group0,
-        'OWL': group0,
-        'PPG': group0,
-        'RAM': group0,
-        'SHE': group0,
-        'SPO': group0,
-        'STU': group0,
-        'VOL': group0,
+        'ADV': [group2, False],
+        'ATL': [group0, True],
+        'CLW': [group2, False],
+        'DUC': [group2, True],
+        'FUT': [group2, False],
+        'GRA': [group0, True],
+        'GUM': [group0, False],
+        'KOR': [group1, True],
+        'MLP': [group0, False],
+        'OWL': [group2, False],
+        'PPG': [group2, False],
+        'RAM': [group0, False],
+        'SHE': [group2, False],
+        'SPO': [group2, False],
+        'STU': [group0, True],
+        'VOL': [group2, False],
     }
 
-    return switcher.get(code, group0)(content)
+    return switcher.get(code, group0)[0](content, switcher[code][1])
 
 if __name__ == "__main__":
     show_paths = initial_setup()
-    convert_all(show_paths, path = ('ATL', 'KOR'))
+    convert_all(show_paths, path = ('GRA', 'GUM', 'MLP', 'RAM', 'STU'))
